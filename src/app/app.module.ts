8c53f4ec8c274fc854cab152c913f4816dec6d65
Audrey <audreyhayes0@gmail.com>
2018-10-01 12:19:29 +0100

    mod files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RowersComponent } from './rowers/rowers.component';
+import { RowerDetailComponent } from './rower-detail/rower-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RowersComponent,
+    RowerDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
